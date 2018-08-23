0d358d107cbdf134ca78f783517f7dc1a2e87c5e
pickrecalled <pickrecalled@163.com>
2018-08-23 17:47:03 +0800

    2.@ComponentScan-自动扫描组件&指定扫描规则

--- src/main/java/com/pickrecalled/config/PersonConfig.java
+++ src/main/java/com/pickrecalled/config/PersonConfig.java
@@ -6,10 +6,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
 @Configuration
+//排除@Controller标注的组件
+//@ComponentScan(value = "com.pickrecalled", excludeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = { Controller.class })})
 
+//只包含@Controller标注的组件，注意需要配置useDefaultFilters = false
+//@ComponentScan(value = "com.pickrecalled", useDefaultFilters = false, includeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = { Controller.class }) })
+
+//JDK8版本@ComponentScan是被@Repeatable注解标签标注的，可以重复出现多次
+/*@ComponentScan(value = "com.pickrecalled", useDefaultFilters = false, includeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = { Controller.class }) })
+@ComponentScan(value = "com.pickrecalled", useDefaultFilters = false, includeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = { Repository.class }) })*/
+
+//扫描包策略标签
 @ComponentScans(value = {
         @ComponentScan(value = "com.pickrecalled", useDefaultFilters = false, includeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = { Controller.class }) }),
-		@ComponentScan(value = "com.pickrecalled", useDefaultFilters = false, includeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = { Service.class }) })
+        @ComponentScan(value = "com.pickrecalled", useDefaultFilters = false, includeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = { Service.class }) })
 })
 public class PersonConfig {
 
